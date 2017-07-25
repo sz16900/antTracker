aef295029798fdf6a3c7d95c1b5e52cd165938bc
Seth <sz16900@my.bristol.ac.uk>
2017-07-25 13:33:15 +0100

    debugging and shortening

--- euclidean.py
+++ euclidean.py
@@ -3,107 +3,146 @@
 
 
 from scipy.spatial import distance
-
-before = list()
-after = list()
+import tempfile
+import sys
+import re
 
 fileName = "tracks.txt"
 
-# For now, I am only going to put two because I know it
-frame = 1
-reset = False
-
 # Euclidean function
-def myEuclidean(before, after, resetNum):
-	idBef = 1
-	idAft = 1
-	finalIdArray = list()
+def myEuclidean(before, after):
+	finalIdDict = {}
+
+	for key, value in before.iteritems():
+		afterDist = {}
+		# I think that there should be some check here, but for now it seems to be working as it is
+		for keyAft, valueAft in after.iteritems():
+			afterDist[keyAft] = distance.euclidean(before[key], after[keyAft])
+		# get the corresponding id
+		after[min(afterDist, key=afterDist.get)] = key
+
+	return after
+
+# For now, I am only going to put two because I know it
+
+def readRestart():
+	frame = 1
+	reset = False
+	theCode = list()
+	before = {}
+	after = {}
+	theIds = list()
+
+	with open(fileName) as myFile:
+		# lets skip the header
+		myFile.next() 
+		for line in myFile:
+
+			if "RESET" in line:
+				line = myFile.next()
+				frame += 1
+				reset = True
+
+
+			ids = (0,0)
+			intList = map(int, line.strip().split())
+
+			if frame != intList[0] and reset ==  True:
+				# print 4
+
+				frame += 1
+				# Perhaps here I run the euclidean distance in a function
+				reset = False
+				# print "BEFORE: ", before
+				# print "After: ", after
+				finalIdDict = myEuclidean(before, after)
+				transcribe(finalIdDict)
+				break
+
+			# This means we are in the frame and checking
+			elif frame == intList[0] and reset ==  False:
+				# print 1
+				theIds.append(intList[1])
+				ids = (intList[2], intList[3])
+				before[intList[1]] = ids
+			# This means we are not on the same frame, move on	
+			elif frame != intList[0] and reset ==  False:
+				# print 2
+				frame += 1
+				before = {}
+				after = {}
+				theIds = list()
+				theIds.append(intList[1])
+				ids = (intList[2], intList[3])
+				before[intList[1]] = ids
+
+			elif frame == intList[0] and reset ==  True:
+				# print 3
+				ids = (intList[2], intList[3])
+				after[intList[1]] = ids
+
+def transcribe(finalIdDict):
+	oldFile = open(fileName, "r")
+
+	t = tempfile.NamedTemporaryFile(mode="r+")
+	# oldFile = open("tracks.txt", "r")
+	# oldFile.next()
 	change = False
 	resetButton = 0
-	for pointBef in before:
-		beforeList = list()
-		for pointAft in after:
-			beforeList.append(distance.euclidean(pointBef, pointAft))
-		print beforeList
-		finalIdArray.append(beforeList.index(min(beforeList)))
-	print finalIdArray
-	print "Here: ", resetNum
-
-	newFile = open("polishedTracks.txt", "a")
-	oldFile = open("tracks.txt", "r")
 	for line in oldFile:
 		if "RESET" in line:
 			resetButton += 1
 			if resetButton > 1:
 				break
 			#re write everythin over and over, try to fix this later
-			line = myFile.next()
+			line = oldFile.next()
 			change = True
 		if change == True:
 			intList = map(int, line.strip().split())
-			# for now, I know its two, but it needs to be dynaimc
-			if intList[1] == 0:
-				intList[1] = finalIdArray[0]
-				newFile.write("%d %d %d %d\r\n" % (intList[0], intList[1], intList[2], intList[3]))
-			elif intList[1] == 1:
-				intList[1] = finalIdArray[1]
-				newFile.write("%d %d %d %d\r\n" % (intList[0], intList[1], intList[2], intList[3]))
+			intList[1] = finalIdDict[intList[1]]
+			t.write("%d %d %d %d\r\n" % (intList[0], intList[1], intList[2], intList[3]))
+
+
+			# # for now, I know its two, but it needs to be dynaimc
+			# if finalIdArray[0][0] == 0 and finalIdArray[0][1] == 0:
+			# 	if switch == 0:
+			# 		intList[1] = theIds[0]
+			# 		t.write("%d %d %d %d\r\n" % (intList[0], intList[1], intList[2], intList[3]))
+			# 		switch += 1
+			# 	elif switch == 1:
+			# 		intList[1] = theIds[1]
+			# 		t.write("%d %d %d %d\r\n" % (intList[0], intList[1], intList[2], intList[3]))
+			# 		switch -= 1
+
+			# # This is not very dynamic. This needs to change. 
+			# else:
+			# 	if switch == 0:
+			# 		intList[1] = finalIdArray[0][0]
+			# 		t.write("%d %d %d %d\r\n" % (intList[0], intList[1], intList[2], intList[3]))
+			# 		switch += 1
+			# 	elif switch == 1:
+			# 		intList[1] = finalIdArray[0][1]
+			# 		t.write("%d %d %d %d\r\n" % (intList[0], intList[1], intList[2], intList[3]))
+			# 		switch -= 1
 		else:
-			newFile.write(line)
-	newFile.close()
+			t.write(line)
+
+	# oldFile.close()
 	oldFile.close()
 
-with open(fileName) as myFile:
-	# lets skip the header
-	myFile.next() 
-	for num, line in enumerate(myFile, 2):
-		if "RESET" in line:
-			print "This is the number: ", num
-			resetNum = num
-			line = myFile.next()
-			frame += 1
-			reset = True
-			
-		ids = list()
-		intList = map(int, line.strip().split())
+	t.seek(0) #Rewind temporary file to beginning
 
-		print intList[0]
+	o = open('tracks.txt', "r+")  #Reopen input file writable
 
-		if frame != intList[0] and reset ==  True:
-			print 4
-
-			frame += 1
-			# Perhaps here I run the euclidean distance in a function
-			reset = False
-			myEuclidean(before, after, resetNum)
-			print resetNum
-			break
-
-		if frame == intList[0] and reset ==  False:
-			print 1
-			ids.append(intList[2])
-			ids.append(intList[3])
-			before.append(ids)
-		elif frame != intList[0] and reset ==  False:
-			print 2
-
-			frame += 1
-			before = list()
-			ids.append(intList[2])
-			ids.append(intList[3])
-			before.append(ids)
-		elif frame == intList[0] and reset ==  True:
-
-			print 3
-
-			ids.append(intList[2])
-			ids.append(intList[3])
-			after.append(ids)
+	#Overwriting original file with temporary file contents
+	
+	for line in t:
+		o.write(line)
 
 
+	t.close() #Close temporary file, will cause it to be deleted 
+	o.close()
 
 
-		# break
-
-print before
-print after
+readRestart()
+# print theCode
